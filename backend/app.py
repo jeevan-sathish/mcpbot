4909f368abe5e0768ad900d1bdf23ff894f6d630
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-16 20:22:11 +0530

    backend wriotten

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,17 @@
-print("hello")
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["POST"])
+def get_data():
+    data = request.get_json()
+    prompt = data.get("prompt")
+
+    return jsonify({
+        "reply": f"You sent: {prompt}"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
 const App = () => {
 
-  const [prompt,setPrompt]=useState("")
-  const [response,setResponse]=useState("")
-  async function handleRequest(){
-    const response = await fetch("",{
-      method:"POST",
-      body:prompt
-    })
+  const [prompt, setPrompt] = useState("")
+  const [response, setResponse] = useState(null)
 
-    const result = response.json()
-    setResponse(result)
+  async function handleRequest() {
+    try {
+
+      const res = await fetch("http://127.0.0.1:5000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          prompt: prompt
+        })
+      })
+
+      const result = await res.json()
+
+      setResponse(result)
+
+    } catch (err) {
+      console.log("error:", err)
+    } finally {
+      console.log("run successfully")
+    }
   }
+
   return (
     <div>
-    <input type="text" onChange={(e)=>setPrompt(e.target.value)} />
-    <button onClick={handleRequest}>send</button>
-    <pre>
-      {JSON.stringify(response,null,2)}
-    </pre>
+
+      <input
+        type="text"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+
+      <button onClick={handleRequest}>
+        Send
+      </button>
+
+      <pre>
+        {JSON.stringify(response, null, 2)}
+      </pre>
 
     </div>
   )
