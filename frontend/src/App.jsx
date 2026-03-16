02b5b964e2ed8b62c6dbf51ad26aaad096914e77
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-16 20:14:00 +0530

    client form created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [prompt,setPrompt]=useState("")
+  const [response,setResponse]=useState("")
+  async function handleRequest(){
+    const response = await fetch("",{
+      method:"POST",
+      body:prompt
+    })
+
+    const result = response.json()
+    setResponse(result)
+  }
   return (
-    <div>App</div>
+    <div>
+    <input type="text" onChange={(e)=>setPrompt(e.target.value)} />
+    <button onClick={handleRequest}>send</button>
+    <pre>
+      {JSON.stringify(response,null,2)}
+    </pre>
+
+    </div>
   )
 }
 
