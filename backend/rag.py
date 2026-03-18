416d321136ecb2f810e2b5d84a2d277cde22762c
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-18 19:25:24 +0530

    install pymupdf from text extract

---
+++ backend/rag.py
@@ -0,0 +1,14 @@
+import fitz  # PyMuPDF
+
+def read_pdf(file_path):
+    text = ""
+    doc = fitz.open(file_path)
+    
+    for page in doc:
+        text += page.get_text()
+    
+    return text
+
+
+data = read_pdf("sample.pdf")
+print(data)
